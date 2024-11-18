0d7143627c8fa18010a9dc27b9b71251dcd7282e
Aristle Monteiro <aristle.jm@psu.edu>
2024-11-18 16:24:26 -0500

    edited silli initial state

--- silli_initial_state.py
+++ silli_initial_state.py
@@ -6,11 +6,11 @@ import pandas as pd
 sc = sill_controls()
 
 #Dimensions of the 2D grid
-x = 300000/20 #m - Horizontal extent of the crust
+x = 300000/300 #m - Horizontal extent of the crust
 y = 8000 #m - Vertical thickness of the crust
 z = 30000 #m - Third dimension for cube
 
-ecks = x*20
+ecks = x*300
 
 dx = 50 #m node spacing in x-direction
 dy = 50 #m node spacing in y-direction
@@ -94,14 +94,38 @@ params = sc.get_silli_initial_thermogenic_state(props_array, dx, dy, dt, 'conv s
 
 tiled_props_array = np.empty((len((sc.prop_dict.keys())),a,bee), dtype = object)
 for i in range(tiled_props_array.shape[0]):
-    tiled_props_array[i] = np.tile(props_array[i],(1,20))
+    tiled_props_array[i] = np.tile(props_array[i],(1,300))
 
 current_time = params[0]
 csv = pd.read_csv('sillcubes/n_sills.csv')
 csv['curr_time'] = current_time
 csv.to_csv('sillcubes/n_sills.csv')
-tiled_params = np.empty((params[1:].shape[0],a,bee))
-for i in range(params[1:].shape[0]):
-    tiled_params[i] = np.tile(params[i+1],(1,20))
-data = pv.StructuredGrid(np.array(params[1:], dtype = float))
-data.save('sillcubes/initial_silli_state.vtk')
+tot_RCO2, props_array, RCO2_silli, Rom_silli, percRo_silli, curr_TOC_silli, W_silli = params[1:]
+ace = np.array([RCO2_silli, Rom_silli, percRo_silli, curr_TOC_silli])
+tiled_params = np.empty((ace.shape[0],a,bee))
+tiled_props_array[sc.TOC_index] = np.tile(curr_TOC_silli,(1,300))
+for i in range(ace.shape[0]):
+    tiled_params[i] = np.tile(ace[i],(1,300))
+tiled_W = np.empty((W_silli.shape[0],a,bee))
+for i in range(W_silli.shape[0]):
+    tiled_W[i] = np.tile(W_silli[i],(1,300))
+
+data = pv.ImageData()
+# Set the dimensions and spacing
+data.dimensions = tiled_params.shape
+data.spacing = (1.0, 1.0, 1.0)  # Adjust spacing as needed
+# Assign the tiled parameters to the point data
+data.point_data['data'] = np.array(tiled_params, dtype=float).flatten(order="F")
+# Print the point data to verify
+print(data.point_data['data'])
+data.save('sillcubes/initial_silli_state_carbon.vtk')
+
+W_data = pv.ImageData()
+W_data.dimensions = W_silli.shape
+W_data.point_data['data'] = W_silli.flatten(order = 'F')
+W_data.save('sillcubes/W_data.vtk')
+
+props_array_vtk = pv.ImageData()
+props_array_vtk.dimensions = tiled_props_array.shape
+props_array_vtk.point_data['data'] = tiled_props_array.flatten(order = 'F')
+props_array_vtk.save('sillcubes/initial_silli_state_properties.vtk')
