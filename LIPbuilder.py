Edited TuLIP to fix RCO2 units to kg/yr

LIPbuilder.py
@@ -18,14 +18,16 @@ target_volume = int(1e6)*int(1e9) #m3/year
 
 fluxes = [int(3e7), int(3e8), int(3e9)] #m3/year
 
-times_csv = pd.read_csv('sillcubes/'+str(format(fluxes[0], '.3e'))+'/300/times.csv')
-time_steps = times_csv['time_steps']
+#times_csv = pd.read_csv('sillcubes/'+str(format(fluxes[0], '.3e'))+'/300/times.csv')
+#time_steps = times_csv['time_steps']
+#tot_RO2 = np.zeros_like(time_steps)
+
 area_per_cube = 300*30 #km
 
 n = 10
 
 
-tot_RO2 = np.zeros_like(time_steps)
+
 mont_RCO2= np.array([])
 total_area = 0
 current_volume = 0 #m3/year
@@ -50,6 +52,10 @@ for j in trange(n):
 
         print(f'Volume is {volume:.3e}')
 
+        times_csv = pd.read_csv('sillcubes/'+str(format(fluxes[0], '.3e'))+str(format(volume, '.3e'))+'/300/times.csv')
+        time_steps = times_csv['time_steps']
+        tot_RCO2 = np.zeros_like(time_steps)
+
         scales = pd.read_csv(load_dir+'scales.csv')
         scale = scales['scale'][itera]
         volume_cube = scales['tot_volume'][itera]
@@ -61,6 +67,9 @@ for j in trange(n):
 
         for l in range(len(time_cube)):
             tot_RCO2 = CO2_adder(time_steps, time_cube[l], tot_RCO2, RCO2_cube[l])
+        
+        cum_CO2
+
         current_volume+=volume_cube
         remaining_volume-=volume
         total_area+=area_per_cube

cooling_run.py
@@ -82,6 +82,7 @@ def cooler(iter, z_index, flux):
     W_silli = W_vtk.point_data['data'].reshape(W_vtk.dimensions)
 
     RCO2_silli = data.point_data['RCO2_silli'].reshape(data.dimensions)
+    RCO2_silli = RCO2_silli.reshape(data.dimensions[0], data.dimensions[1])/props_array[sc.dense_index]
     Rom_silli = data.point_data['Rom_silli'].reshape(data.dimensions)
     percRo_silli = data.point_data['percRo_silli'].reshape(data.dimensions)
     curr_TOC_silli = data.point_data['curr_TOC_silli'].reshape(data.dimensions)
@@ -104,7 +105,7 @@ def cooler(iter, z_index, flux):
     #RCO2_vtk = pv.read('sillcubes/RCO2.vtk')
     tot_RCO2 = []#list(pd.read_csv('sillcubes/tot_RCO2.csv'))
     carbon_model_params = [tot_RCO2, props_array, RCO2_silli, Rom_silli, percRo_silli, curr_TOC_silli, W_silli]
-    post_cooling_time = 0#30000 #years
+    post_cooling_time = 30000 #years
     end_time = np.array(empl_times)[-1]+post_cooling_time
     print(f'End time is {end_time}')
     time_steps1 = np.arange(current_time,np.array(empl_times)[-1],dt)
@@ -162,7 +163,8 @@ flux2 = int(3e9)
 
 
 factor = np.random.randint(1, int(0.9*c//2), 4)
-z_index = [c//2, c//2+factor[0], c//2+factor[1], c//2-factor[2], c//2-factor[3]]
+#z_index = [c//2, c//2+factor[0], c//2+factor[1], c//2-factor[2], c//2-factor[3]]
+z_index = [191, 284, 300, 493, 506]
 pairs = itertools.product(iter, z_index, fluxy)
 
 #pairs = pairs+pairs2
@@ -192,11 +194,7 @@ for flux in fluxy:
             np.save(load_dir+'/slice_volumes/sillcube'+str(volumes)+'_'+str(z_indexs)+'.npy', sillsquare)
 print(f'slices are {z_index}')
 
-
-
-
-
-#Parallel(n_jobs = 30)(delayed(cooler)(iter, z_indexs, fluxy) for iter, z_indexs, fluxy in pairs)
+Parallel(n_jobs = 30)(delayed(cooler)(iter, z_indexs, fluxy) for iter, z_indexs, fluxy in pairs)
 Parallel(n_jobs = 30)(delayed(cooler)(iter2, z_indexs, flux2) for z_indexs in z_index)
 
 '''

cooling_run2.py
@@ -10,7 +10,7 @@ import pdb
 
 sc = sill_controls()
 
-def cooler(load_dir, z_index):
+def cooler(load_dir, z_index, iter):
     def truncate(number):
         # Convert the number to a string
         number_str = str(number)
@@ -27,8 +27,8 @@ def cooler(load_dir, z_index):
         
         # Convert the truncated string back to a float
         return float(truncated_str)
-    iter = 1
-    flux = int(3e7)
+    #iter = 1
+    flux = int(3e8)
 
     #Dimensions of the 2D grid
     x = 300000 #m - Horizontal extent of the crust
@@ -127,7 +127,7 @@ def cooler(load_dir, z_index):
         else:
             print(f'Emplacement time step {j} is at {np.where(time_steps==empl_times[j])[0]}')
 
-    dir_save = load_dir+'/'+str(z_index)
+    ##dir_save = load_dir+'/'+str(format(volumes,'.3e'))+'/'+str(z_index)
     os.makedirs(dir_save, exist_ok = True)
     timeframe = pd.DataFrame(time_steps[1:], columns=['time_steps'])
     print(len(timeframe['time_steps']))
@@ -151,11 +151,12 @@ a = int(y//dy) #Number of rows
 b = int(x//dx) #Number of columns
 c = int(z//dz) # Number of columns in z direction
 
-load_dirs = ['sillcubes/3.000e+07/half/', 'sillcubes/3.000e+07/middle60/']
+load_dirs = ['sillcubes/3.000e+08/half/', 'sillcubes/3.000e+08/middle60/']
 
-factor = np.random.randint(1, int(0.9*c//2), 4)
-z_index = [c//2, c//2+factor[0], c//2+factor[1], c//2-factor[2], c//2-factor[3]]
-
+#factor = np.random.randint(1, int(0.9*c//2), 4)
+#z_index = [c//2, c//2+factor[0], c//2+factor[1], c//2-factor[2], c//2-factor[3]]
+z_index = [89, 280, 300, 483, 555]
+itera = [0,1,2]
 for load_dir in load_dirs:
     os.makedirs(load_dir+'/slice_volumes', exist_ok=True)
     for filename in os.listdir(os.path.join(load_dir, 'slice_volumes')):
@@ -163,16 +164,17 @@ for load_dir in load_dirs:
         if os.path.isfile(file_path):
             os.remove(file_path)
     n_sills_dataframe = pd.read_csv(load_dir+'/n_sills.csv')
-    volumes = float(n_sills_dataframe['volumes'][1])
-    sillcube = np.load(load_dir+'/sillcube'+str(volumes)+'.npy', allow_pickle=True)
-    for z_indexs in z_index:
+    for iters in itera:
+        volumes = float(n_sills_dataframe['volumes'][iters])
+        sillcube = np.load(load_dir+'/sillcube'+str(volumes)+'.npy', allow_pickle=True)
+        for z_indexs in z_index:
             sillsquare = sillcube[z_indexs]
             np.save(load_dir+'/slice_volumes/sillcube'+str(volumes)+'_'+str(z_indexs)+'.npy', sillsquare)
 
 print(f'slices are {z_index}')
 
-pairs = itertools.product(load_dirs, z_index)
+pairs = itertools.product(load_dirs, z_index, itera)
 
 
 
-Parallel(n_jobs = 30)(delayed(cooler)(load_dir, z_indexs) for load_dir, z_indexs in pairs)
+Parallel(n_jobs = 30)(delayed(cooler)(load_dir, z_indexs, iters) for load_dir, z_indexs, iters in pairs)