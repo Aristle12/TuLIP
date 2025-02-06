00c43ab56a8dabfdf9b75cd3835df32866a90e2c
Aristle12 <46570490+Aristle12@users.noreply.github.com>
2025-02-05 22:13:56 -0500

    Cleaned up cooling_run
    Attempting to reduce memory usage. Main diagnostics pending

--- cooling_run.py
+++ cooling_run.py
@@ -46,13 +46,6 @@ def cooler(iter, z_index, flux):
     z = 30000 #m - Third dimension for cube
 
 
-
-    #Temp at the surface
-    T_surf = 0 #deg C
-
-    #Magmatic temperature
-    T_mag = 1000 #deg C
-
     #Initializing diffusivity field
 
 
@@ -92,14 +85,8 @@ def cooler(iter, z_index, flux):
 
     sillsquare = np.load(load_dir+'/slice_volumes/sillcube'+str(volumes)+'_'+str(z_index)+'.npy', allow_pickle=True)
     
-    print(sillsquare[sillsquare!=''])
-    
     emplacement_params = pd.read_csv(load_dir+'/emplacement_params'+str(volumes)+'.csv')
     empl_times = np.array(emplacement_params['empl_times'])
-    empl_heights = emplacement_params['empl_heights']
-    x_space = emplacement_params['x_space']
-    width = emplacement_params['width']
-    thickness = emplacement_params['thickness']
 
 
 
@@ -113,29 +100,25 @@ def cooler(iter, z_index, flux):
     time_steps2 = np.arange(np.array(empl_times)[-1], end_time+dt, dt)
     time_steps = np.append(time_steps1, time_steps2)
 
-    if time_steps[-1]<empl_times[-1]:
-        raise ValueError(f'Time history stops at {time_steps[-1]} before last emplacement {empl_times[-1]}')
+    if time_steps[-1]<np.array(empl_times)[-1]:
+        raise ValueError(f'Time history stops at {time_steps[-1]} before last emplacement {empl_times[-1]} for {flux, volumes} and {z_index}')
     volume_params = [flux, volumes]
 
-    switch = False
-    empl_times = np.array([truncate(number) for number in empl_times])
-    emplacement_params = [empl_times, empl_heights, x_space, width, thickness]
-    time_steps = np.array([truncate(number) for number in time_steps])
+    #empl_times = np.array([truncate(number) for number in empl_times])
+    #time_steps = np.array([truncate(number) for number in time_steps])
     for j in range(len(empl_times)):
         if len(np.where(time_steps==empl_times[j])[0])==0:
             min_diff = np.min(np.abs(time_steps - empl_times[j]))
-            print(f'Sill {j} is not in time_steps and difference to closest time step is {min_diff}')
+            print(f'Sill {j} is not in time_steps and difference to closest time step is {format(min_diff, '.2e')}')
             time_index = np.where(min_diff==np.abs(time_steps - empl_times[j]))[0]
             if min_diff<dt:
                 empl_times[j] = time_steps[time_index]
                 print(f'Emplacement time for sill {j} has been readjusted and is now at {np.where(time_steps==empl_times[j])[0]}')
         else:
             print(f'Sill {j} is at {np.where(time_steps==empl_times[j])[0]}')
-    breakpoint
     dir_save = 'sillcubes/'+str(format(flux, '.3e'))+'/'+str(format(volumes, '.3e'))+'/'+str(z_index)
     os.makedirs(dir_save, exist_ok = True)
     timeframe = pd.DataFrame(time_steps[1:], columns=['time_steps'])
-    print(len(timeframe['time_steps']))
     timeframe.to_csv(dir_save+'/times.csv')
 
     current_time = np.round(current_time, 3)
@@ -199,61 +182,3 @@ Parallel(n_jobs = 20)(delayed(cooler)(iter, z_indexs, fluxy) for iter, z_indexs,
 #Parallel(n_jobs = 30)(delayed(cooler)(iter2, z_indexs, fluxy) for z_indexs in z_index)
 
 #cooler(0, 191, int(3e9))
-
-'''
-factor = 10
-saving_props_array = np.empty((props_total_array.shape[0]//factor +1,props_total_array.shape[1],a,b), dtype = object)
-saving_time_steps = np.empty(len(time_steps)//factor+1)
-T_field = np.empty((len(time_steps)//factor+1,a,b))
-density_field = np.empty_like(T_field)
-porosity_field = np.empty_like(T_field)
-TOC_field = np.empty_like(T_field)
-rock_field = np.empty_like(T_field, dtype = object)
-
-for i in range(len(time_steps)):
-    if i%factor ==0:
-        saving_time_steps[i//factor] = time_steps[i]
-        saving_props_array[i//factor] = props_total_array[i]
-        T_field[i//factor] = props_total_array[i,sc.Temp_index]
-        density_field[i//factor] = props_total_array[i,sc.dense_index]
-        porosity_field[i//factor] = props_total_array[i,sc.poros_index]
-        TOC_field[i//factor] = props_total_array[i,sc.TOC_index]
-        rock_field[i//factor] = props_total_array[i, sc.rock_index]
-
-T_data = pv.ImageData(dimensions=(a,b, len(saving_time_steps)), spacing = (dx,dy, dt))
-density_data = pv.ImageData(dimensions=(a,b, len(saving_time_steps)), spacing = (dx,dy, dt))
-porosity_data = pv.ImageData(dimensions=(a,b, len(saving_time_steps)), spacing = (dx,dy, dt))
-TOC_data = pv.ImageData(dimensions=(a,b, len(saving_time_steps)), spacing = (dx,dy, dt))
-rock_data = pv.ImageData(dimensions=(a,b, len(saving_time_steps)), spacing = (dx,dy, dt))
-
-
-for t in range(len(saving_time_steps)):
-    T_data.point_data[f'data_{saving_time_steps[t]}'] = T_field[t].flatten()
-    density_data.point_data[f'data_{t}'] = density_field[t].flatten
-    porosity_data.point_data[f'data_{t}'] = porosity_field[t].flatten
-    TOC_data.point_data[f'data_{t}'] = TOC_field[t].flatten
-    rock_data.point_data[f'data_{t}'] = rock_field[t].flatten
-
-
-
-T_data.time_values = saving_time_steps
-density_data.time_values = saving_time_steps
-porosity_data.time_values = saving_time_steps
-TOC_data.time_values = saving_time_steps
-rock_data.time_values = saving_time_steps
-
-
-T_data.point_data['data'] = np.array(T_field, dtype = float).flatten()
-density_data.point_data['data'] = np.array(density_field, dtype = float).flatten()
-porosity_data.point_data['data'] = np.array(porosity_field, dtype = float).flatten()
-TOC_data.point_data['data'] = np.array(TOC_field, dtype = float).flatten()
-rock_data.point_data['data'] = np.array(rock_field).flatten()
-
-
-os.makedirs(dir, exist_ok=True)
-T_data.save(dir+'/T_data.vtk')
-density_data.save(dir+'/density_data.vtk')
-porosity_data.save(dir+'/porosity_data.vtk')
-TOC_data.save(dir+'/TOC_data.vtk')
-rock_data.save(dir+'/rock_data.vtk')
-'''
