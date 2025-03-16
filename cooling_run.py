86a725b5da9a6c9a36d7716158c5b136ad7d186f
Aristle Monteiro <aristle.jm@psu.edu>
2025-03-16 13:50:55 -0400

    Minor edits to cooling_run

--- cooling_run.py
+++ cooling_run.py
@@ -45,12 +45,12 @@ file_path_dir = 'sillcubes/'
 footnote1 = 'LH_2'
 footnote2 = 'noLH_2'
 footnote3 = 'noLHCP2_2'
-post_cooling_time = 500 #years
-fluxy_list = [int(3e9)]#, int(3e8), int(3e7), int(3*10**(7.5)), int(3*10**(8.5))]
-iter_list = [0]#, 1, 2, 3, 4]
-z_index_list = [191, 284]#, 300, 493, 506]
+post_cooling_time = 30000 #years
+fluxy_list = [int(3e9), int(3e8), int(3e7), int(3*10**(7.5)), int(3*10**(8.5))]
+iter_list = [0, 1, 2, 3, 4]
+z_index_list = [191, 284, 300, 493, 506]
 pairs = itertools.product(iter_list, z_index_list, fluxy_list)
-saving_factor = [10]
+saving_factor = [100]
 
 '''
 redo_flux = [int(3e9), int(3*10**8.5), int(3e8), int(3e8)]
@@ -61,6 +61,11 @@ z_index = [191, 284, 300, 493, 506]
 tiled_z = np.tile(z_index, 5)
 pairs = zip(redo_iter, tiled_z, redo_flux)
 '''
+Parallel(n_jobs = 20)(delayed(util.cooler)(iter_1, z_index_1, fluxy_1,sc=sc_latent_heat,diff_val=diff_val,
+                                      temp_grad_base = temp_grad_base,
+                                      file_path_dir=file_path_dir, post_cooling_time = post_cooling_time,
+                                      x=x,y=y,dx=dx,dy=dy, saving_factor = saving_factor) for iter_1, z_index_1, fluxy_1 in pairs)
+
 
 ### Run this only once for a model set###
 #sc.generate_sill_2D_slices(fluxy_list,iter_list,z_index_list)
@@ -75,12 +80,12 @@ Parallel(n_jobs = 30)(delayed(util.cooler)(iter_1, z_index_1, fluxy_1,sc=sc_nola
                                       temp_grad_base = temp_grad_base,
                                       file_path_dir=file_path_dir, post_cooling_time = post_cooling_time,
                                       x=x,y=y,dx=dx,dy=dy, save_dir_footnote=footnote3, saving_factor = saving_factor) for iter_1, z_index_1, fluxy_1 in pairs)
-'''
+
 Parallel(n_jobs = 30)(delayed(util.cooler)(iter_1, z_index_1, fluxy_1,sc=sc_nolatent_heat,diff_val=diff_val,
                                       temp_grad_base = temp_grad_base,
                                       file_path_dir=file_path_dir, post_cooling_time = post_cooling_time,
                                       x=x,y=y,dx=dx,dy=dy, save_dir_footnote=footnote2, saving_factor = saving_factor) for iter_1, z_index_1, fluxy_1 in pairs)
-
+'''
 #Parallel(n_jobs = 30)(delayed(cooler)(iter2, z_indexs, fluxy) for z_indexs in z_index)
 
 #cooler(0, 191, int(3e9))
