bb5859557f6afecbc88fb7aaab6c7eb1e887adf4
Aristle Monteiro <aristle.jm@psu.edu>
2026-03-25 17:49:04 -0400

    Fixed sill_network

--- sill_network.py
+++ sill_network.py
@@ -24,7 +24,7 @@ for i in range(0, len(fluxs)):
         conf_mat = np.zeros((n_sills[j], n_sills[j]))
         sill_distance = pd.read_csv(load_dir+'sill_distances.csv')
         choose = np.where(sill_distance['distance']<1e31)[0]
-        sill_distance['criteria'] = np.array(sill_distance['width of closest sill'])
+        sill_distance['criteria'] = 1.5*np.array(sill_distance['width of closest sill'])
         for k in range(n_sills[j]):
             if k in sill_distance['sills'].values:
                 row = sill_distance.loc[sill_distance['sills'] == k].iloc[0]
